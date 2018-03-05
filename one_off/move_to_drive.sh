064275e19d781e9034f3cb8001ce647cfd049ec4
Bryant Chow <bryant.h.c@gmail.com>
2018-03-05 17:28:34 +1300

    full on rranging

--- one_off/move_to_drive.sh
+++
@@ -1,2 +0,0 @@
-cp -rn /seis/prj/fwi/bchow/spectral/output_plots/ /run/media/bchow/convolution/
-cp -rn ~/Documents/papers/ /run/media/bchow/
--- one_off/spectro_batch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# shell script, for station list, download station data then create spectrogram
-for station in TBAS GWTS GISS GKBS TUDS WFSS KFHS MWDS ORCS TTHS TPPS WAIS TIRS RPCS ROTS TUHS NSPS HNPS WPWS TDHS GHHS NAAS NCDS NGHS NCHS HCDS KAFS OPCS WKHS MWFS WAKS THHS UTKS MNGS TBCS MMCS HBHS TKHS
-do
-python make_spectrogram.py "${station}" z
-done
