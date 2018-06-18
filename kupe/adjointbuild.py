6587526119a133341905f06f083b320a43f9a522
Bryant Chow <bryant.h.c@gmail.com>
2018-06-18 14:59:49 +1200

    adjointbuild working GNS

--- kupe/adjointbuild.py
+++ kupe/adjointbuild.py
@@ -28,7 +28,7 @@ mpl.rcParams['lines.linewidth'] = 1
 
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
-warnings.filterwarnings("ignore", category=mpl.cbook.mplDeprecation)    
+warnings.filterwarnings("ignore", category=mpl.cbook.mplDeprecation)
 
 # ============================ HELPER FUNCTIONS ================================
 def find_BAz(inv,event):
@@ -42,12 +42,12 @@ def find_BAz(inv,event):
     dist,Az,BAz = gps2dist_azimuth(event_lat,event_lon,station_lat,station_lon)
 
     return BAz
-    
-def breakout_stream(st):    
+
+def breakout_stream(st):
     """get observed and synthetic parts of a stream object
-    assumes that 'st' contains all observation and synthetic traces and that 
+    assumes that 'st' contains all observation and synthetic traces and that
     they follow the naming scheme: obs="HH?"", syn="BX?"
-    
+
     :type st: obspy.stream
     :param st: stream to be separated
     :rtype obs_stream: obspy.stream
@@ -58,7 +58,7 @@ def breakout_stream(st):
     # break out streams into individuals
     obs_stream = st.select(channel="HH?")
     syn_stream = st.select(channel="BX?")
-    
+
     return obs_stream, syn_stream
 
 # ============================= MAIN FUNCTIONS =================================
@@ -66,13 +66,13 @@ def initial_data_gather(PD):
     """gather event information, observation and synthetic traces.
     preprocess all traces accordingly and return one stream object with 6 traces
     stolen and modified from obsynth.py
-    
+
     TODO:
-        +by default grabs all components, could be modified to only grab 
+        +by default grabs all components, could be modified to only grab
         necessary components, if the speedup is worthwhile
-        +change the stream length by the theoretical arrival times or 
+        +change the stream length by the theoretical arrival times or
         source-receiver distance (?)
-    
+
 
     :type PD: dictionary
     :param PD: parameter dictionary
@@ -84,11 +84,11 @@ def initial_data_gather(PD):
     :return event: event information
     """
     # station information
-    net,sta,loc,cha = PD["code"].split('.')    
+    net,sta,loc,cha = PD["code"].split('.')
 
     # grab synthetic data locally, decide
     syntheticdata_path = join(pathnames()['syns'],PD["event_id"],'')
-    
+
     syntheticdata = Stream()
     for c in ["N","E","Z"]:
         syntheticdata_filename = "{n}.{s}.BX{co}.semv.mseed".format(n=net,
@@ -97,22 +97,22 @@ def initial_data_gather(PD):
         syntheticdata += read(join(syntheticdata_path,syntheticdata_filename))
 
     # grab observation data
-    observationdata,inv,cat = getdata.event_stream(code=PD["code"],
+    observationdata,inv,event = getdata.event_stream(code=PD["code"],
                                                     event_id=PD["event_id"],
                                                     startpad=0,
                                                     endpad=180)
-    event = cat[0]
-    
+    # event = cat[0]
+
     if not observationdata:
         print("No observation data")
         return None
-        
+
     # rotate to theoretical backazimuth if necessary
     if PD["component"] == ("R" or "T"):
         BAz = find_BAz(inv,event)
         observationdata_proc.rotate(method='NE->RT',back_azimuth=BAz)
         syntheticdata_proc.rotate(method='NE->RT',back_azimuth=BAz)
-        
+
     # only grab the necessary streams to save on processing
     observationdata = observationdata.select(component=PD["component"])
     syntheticdata = syntheticdata.select(component=PD["component"])
@@ -121,19 +121,19 @@ def initial_data_gather(PD):
     observationdata_proc = procmod.preprocess(observationdata,
                                                 inv=inv,
                                                 output=PD["output"])
-                                            
+
     # synthetic moment-tensor information
     time_shift, half_duration = synmod.tshift_halfdur(PD["event_id"])
-    
+
     # if GCMT solution doesn't exist, timeshift isn't possible
     if time_shift:
         syntheticdata_preproc = synmod.stf_convolve(st=syntheticdata,
                                                  half_duration=half_duration,
                                                  time_shift=time_shift)
-                                                 
+
     syntheticdata_proc = procmod.preprocess(syntheticdata_preproc,inv=None,
                                             output=PD["output"])
-    
+
 
     # combine and trim to common time
     st_IDG = observationdata_proc + syntheticdata_proc
@@ -148,58 +148,75 @@ def initial_data_gather(PD):
 
     return st_IDG,inv,event
 
-def run_pyflex(PD,st,inv,event,plot=False):
+def run_pyflex(PD,st,inv,event,plot=False,config="UAF"):
     """use pyflex to grab windows, current config set to defaults found on docs
-    
+
     :type PD: dictionary
     :param PD: parameter sets
     :rtype windows: pyflex.window object
-    :return windows: windows containing selected timespans where waveforms 
+    :return windows: windows containing selected timespans where waveforms
     have acceptable match, also contains information about the window (see docs)
-    """                               
+    """
     obs,syn = breakout_stream(st)
-    
-    # configs currently set to doc defaults
-    config = pyflex.Config(min_period=PD["bounds"][0],
-                           max_period=PD["bounds"][1],
-                           stalta_waterlevel=0.08,
-                           tshift_acceptance_level=15.0,
-                           dlna_acceptance_level=1.0,
-                           cc_acceptance_level=0.8,
-                           c_0=0.7,
-                           c_1=4.0,
-                           c_2=0.0,
-                           c_3a=1.0,
-                           c_3b=2.0,
-                           c_4a=3.0,
-                           c_4b=10.0
-                           )
-                           
+
+    # DEFAULT CONFIG
+    if config == "default":
+        config = pyflex.Config(min_period=PD["bounds"][0],
+                               max_period=PD["bounds"][1],
+                               stalta_waterlevel=0.08,
+                               tshift_acceptance_level=15.0,
+                               dlna_acceptance_level=1.0,
+                               cc_acceptance_level=0.8,
+                               c_0=0.7,
+                               c_1=4.0,
+                               c_2=0.0,
+                               c_3a=1.0,
+                               c_3b=2.0,
+                               c_4a=3.0,
+                               c_4b=10.0
+                               )
+        # UAF CONFIG
+    elif config == "UAF":
+        config = pyflex.Config(min_period=PD["bounds"][0],
+                               max_period=PD["bounds"][1],
+                               stalta_waterlevel=0.18,
+                               tshift_acceptance_level=4.0,
+                               dlna_acceptance_level=1.5,
+                               cc_acceptance_level=0.71,
+                               c_0=0.7,
+                               c_1=2.0,
+                               c_2=0.0,
+                               c_3a=3.0,
+                               c_3b=2.0,
+                               c_4a=2.5,
+                               c_4b=12.0)
+
+
     windows = pyflex.select_windows(observed=obs,
                                     synthetic=syn,
                                     config=config,
-                                    event=event,
+                                    # event=event, # not working for some reason
                                     station=inv,
                                     plot=plot)
     if not windows:
         print("Empty windows")
         return None
-    
+
     return windows
-    
+
 def run_pyadjoint(PD,st,windows,output_path=None,plot=False):
     """function to call pyadjoint with preset configurations
     ++not in the docs:
     in pyadjoint.calculate_adjoint_source: window needs to be a list of lists,
     with each list containing the [left_window,right_window] where each window
     argument is given in seconds
-    
+
     :type PD: dictionary
     :param PD: parameter sets
     :type st: obspy.stream
     :param st: stream to be separated
     :type windows: pyflex.window object
-    :param windows: windows containing selected timespans where waveforms 
+    :param windows: windows containing selected timespans where waveforms
     have acceptable match, also contains information about the window (see docs)
     :type output_path: str
     :param output_path: where to save the adjoint source, if none, no saving
@@ -208,28 +225,36 @@ def run_pyadjoint(PD,st,windows,output_path=None,plot=False):
     obs,syn = breakout_stream(st)
     net,sta,loc,cha = PD["code"].split('.')
     cha=cha[:2]+PD["component"]
-    
+
     # collect all windows into a single list object
     adjoint_windows = []
     delta = st[0].stats.delta
     for win in windows:
-        adj_win = [win.left*delta,win.right*delta] 
+        adj_win = [win.left*delta,win.right*delta]
         adjoint_windows.append(adj_win)
-    
+
     config = pyadjoint.ConfigWaveForm(min_period=PD["bounds"][0],
-                                      max_period=PD["bounds"][1])
-                                      # taper_type="hann",
-                                      # taper_percentage=0.15)
-                                      
+                                      max_period=PD["bounds"][1],
+                                      taper_type="hann",
+                                      taper_percentage=0.15)
+    config_adj = pyadjoint.ConfigCrossCorrelation(min_period=PD["bounds"][0],
+                                                  max_period=PD["bounds"][1],
+                                                  taper_type='hann',
+                                                  taper_percentage=0.3,
+                                                  measure_type='dt',
+                                                  dt_sigma_min=1.0,
+                                                  dlna_sigma_min=0.5)
+
+
     adj_src = pyadjoint.calculate_adjoint_source(
-                                 adj_src_type="waveform_misfit",
+                                 adj_src_type="cc_traveltime_misfit",
                                  observed=obs,
                                  synthetic=syn,
-                                 config=config,
+                                 config=config_adj,
                                  window=adjoint_windows,
                                  plot=plot
                                  )
-        
+
     if output_path:
         output_file = os.path.join(output_path,
                                    "{NET}.{STA}.{CHA}.{EVENT_ID}.adj".format(
@@ -240,12 +265,12 @@ def run_pyadjoint(PD,st,windows,output_path=None,plot=False):
         adj_src.write(output_file,
                       format="SPECFEM",
                       time_offset=0)
-                                     
+
     return adj_src
-    
+
 def bobTheBuilder():
     """main processing script
-    
+
     ++intra-function parameters and choices:
     :type EVENT_ID: str
     :param EVENT_ID: GEONET event ID
@@ -265,18 +290,21 @@ def bobTheBuilder():
     """
     # =============== PARAMETER SET ===============
     EVENT_IDS = ["2014p240655"]
-    STATION_NAMES = ['NZ.BFZ','NZ.BKZ','NZ.HAZ','NZ.HIZ','NZ.KNZ','NZ.MRZ',
-                     'NZ.MWZ','NZ.OPRZ','NZ.PUZ','NZ.PXZ','NZ.RTZ','NZ.TLZ',
-                     'NZ.TOZ','NZ.TSZ','NZ.VRZ','NZ.WAZ']
+    # STATION_NAMES = ['NZ.BFZ','NZ.BKZ','NZ.HAZ','NZ.HIZ','NZ.KNZ','NZ.MRZ',
+    #                  'NZ.MWZ','NZ.OPRZ','NZ.PUZ','NZ.PXZ','NZ.RTZ','NZ.TLZ',
+    #                  'NZ.TOZ','NZ.TSZ','NZ.VRZ','NZ.WAZ']
+    STATION_NAMES = ['NZ.HAZ']
     MINIMUM_FILTER_PERIOD = 3
     MAXIMUM_FILTER_PERIOD = 30
     COMPONENT = "Z"
     UNIT_OUTPUT = "VEL"
+    # ADJOINT_TYPE = "cc_traveltime_misfit"
+    # CONFIG = "default"
     ADJ_SRC_OUTPUT_PATH = pathnames()["kupedata"] + "ADJOINTSOURCES"
     ADJ_SRC_OUTPUT_PATH = None
     PLOT = True
     # =============== PARAMETER SET ===============
-    
+
     for EVENT_ID in EVENT_IDS:
         for STATION_NAME in STATION_NAMES:
             print(STATION_NAME)
@@ -288,18 +316,17 @@ def bobTheBuilder():
                         "component":COMPONENT,
                         "output":UNIT_OUTPUT
                         }
-                                            
+
             # PROCESSING
-            st,inv,event = initial_data_gather(PAR_DICT)           
+            st,inv,event = initial_data_gather(PAR_DICT)
             windows = run_pyflex(PAR_DICT,st,inv,event,plot=PLOT)
             if not windows:
                 continue
             adj_src = run_pyadjoint(PAR_DICT,st,windows,
                                     output_path=ADJ_SRC_OUTPUT_PATH,
                                     plot=PLOT)
-    
-            
+
+
 # =================================== MAIN ====================================
 if __name__ == "__main__":
     bobTheBuilder()
-    
