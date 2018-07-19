a0cc0af2b94f0630dced27bfe401fab9793a75fc
Bryant Chow <bryant.h.c@gmail.com>
2018-07-19 13:25:39 +1200

    remade ppsdplot internal structure

--- kupe/tools/estimateMRP.py
+++ kupe/tools/estimateMRP.py
@@ -12,8 +12,12 @@ from plotmod import pretty_grids
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-mpl.rcParams['font.size'] = 8
+mpl.rcParams['font.size'] = 12
 mpl.rcParams['lines.linewidth'] = 1
+
+def get_station_names():
+    """
+    """
 
 def estimate_MRP(sta):
     """the function
@@ -36,13 +40,12 @@ def estimate_MRP(sta):
     st[1].stats.delta=.0107
     commonend = min([st[0].stats.endtime,st[1].stats.endtime])
     commonSR = min([st[0].stats.sampling_rate,st[1].stats.sampling_rate])
-    import ipdb;ipdb.set_trace()
     for tr in st:
         tr.trim(tr.stats.starttime,commonend)
         tr.resample(commonSR)
-        
+
     # st[1].data = st[1].data[50:]
-        
+
     # setup plot
     f,ax = plt.subplots()
     pretty_grids(ax)
@@ -57,20 +60,35 @@ def estimate_MRP(sta):
 
 
     step = 0
+    # plot raw seismogram
+    st_filter = st.copy()
     st.filter('bandpass',freqmin=1/100,freqmax=1)
+    for tr in st_filter:
+        tr.data /= tr.data.max()
+    plt.annotate('raw',xy=(0,step))
+    plt.plot(t_ngll5,st_filter.select(location='NGLL5')[0].data,'k',label='NGLL5')
+    plt.plot(t_ngll7,st_filter.select(location='NGLL7')[0].data,'r',label='NGLL7')
+    step += 1.5
+
+    # plot steps of lowpass filters
     for lowpass in np.arange(0.5,8,0.5):
         st_filter = st.copy()
-        st_filter.filter('lowpass',freq=lowpass)
+        st_filter.filter('lowpass',freq=1/lowpass)
         for tr in st_filter:
             tr.data /= tr.data.max()
             tr.data += step
 
         plt.plot(t_ngll5,st_filter.select(location='NGLL5')[0].data,'k',label='NGLL5')
         plt.plot(t_ngll7,st_filter.select(location='NGLL7')[0].data,'r',label='NGLL7')
-        plt.annotate('lowpass @ {}'.format(lowpass),xy=(0,step))
+        plt.annotate('lowpass @ {}s'.format(lowpass),xy=(0,step))
 
-        step += 1.0
+        step += 1.5
 
+    # final plot adjustments
+    plt.title('2018p130600 Minimum resolveable period estimation\n'
+                                    'NGLL5 (black) vs. NGLL7 (red) Z component')
+    plt.gca().axes.get_yaxis().set_ticks([])
+    plt.xlabel('Time (s)')
     plt.show()
 
 if __name__ == "__main__":
