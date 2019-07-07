7a43d73499a6feb7f6a0ab5e58d8ae1fe6cf5cf9
Bryant Chow <bryant.h.c@gmail.com>
2019-07-07 22:33:00 +1200

    checkerboard coming together

--- simulations/meshing/checkerboardiphy.py
+++ simulations/meshing/checkerboardiphy.py
@@ -1,12 +1,13 @@
 """
 Generate a checkerboard tomography model from an existing tomography .xyz file
-For use with Specfem3D Cartesian
+For use with Specfem3D Cartesian.
 """
 import os
 import numpy as np
+from scipy import signal
 
 
-def xyz_reader(filepath):
+def xyz_reader(xyz_fid, save=True):
     """
     read .xyz file used as input for specfem and return information
     header parsing specifically related to specfem
@@ -15,99 +16,124 @@ def xyz_reader(filepath):
     -origin and endpoints are the coordinates in meters
     -spacing given in units of meters for UTM projections
     -nx, ny, nz = [(end_x - orig_x)/spacing_x] + 1
-    :param filepath:
-    :return:
+    :type xyz_fid: str
+    :param xyz_fid: file to read in
+    :type save: bool
+    :param save: to save the read files into numpy arrays
+    :rtype header: dict
+    :return header: a dictionary with relevant header information
+    :rtype data: np.ndarray
+    :return data: all the data contained in the xyz file
     """
-    with open(filepath) as f:
-        lines = f.readlines()
+    if os.path.exists(xyz_fid + ".npy") and os.path.exists(xyz_fid + ".npz"):
+        header = np.load(xyz_fid + ".npz")
+        data = np.load(xyz_fid + ".npy")
+        return header, data
+    else:
+        with open(xyz_fid) as f:
+            lines = f.readlines()
 
-    # parse the header and make sure the values are floats
-    orig_x, orig_y, orig_z, end_x, end_y, end_z = \
-        [float(_) for _ in lines[0].strip().split()]
-    spacing_x, spacing_y, spacing_z = \
-        [float(_) for _ in lines[1].strip().split()]
-    nx, ny, nz = [float(_) for _ in lines[2].strip().split()]
-    vp_min, vp_max, vs_min, vs_max, rho_min, rho_max = \
-        [float(_) for _ in lines[3].strip().split()]
+            # parse the header and make sure the values are floats
+            orig_x, orig_y, orig_z, end_x, end_y, end_z = \
+                [float(_) for _ in lines[0].strip().split()]
+            spacing_x, spacing_y, spacing_z = \
+                [float(_) for _ in lines[1].strip().split()]
+            nx, ny, nz = [float(_) for _ in lines[2].strip().split()]
+            vp_min, vp_max, vs_min, vs_max, rho_min, rho_max = \
+                [float(_) for _ in lines[3].strip().split()]
 
-    parsed_header = {"orig_x": orig_x, "orig_y": orig_y, "orig_z": orig_z,
-                     "end_x": end_x, "end_y": end_y, "end_z": end_z,
-                     "spacing_x": spacing_x, "spacing_y": spacing_y,
-                     "spacing_z": spacing_z, "nx": nx, "ny": ny, "nz": nz,
-                     "vp_min": vp_min, "vp_max": vp_max, "vs_min": vs_min,
-                     "vs_max": vs_max, "rho_min": rho_min, "rho_max": rho_max,
+            header = {"orig_x": orig_x, "orig_y": orig_y, "orig_z": orig_z,
+                      "end_x": end_x, "end_y": end_y, "end_z": end_z,
+                      "spacing_x": spacing_x, "spacing_y": spacing_y,
+                      "spacing_z": spacing_z, "nx": nx, "ny": ny, "nz": nz,
+                      "vp_min": vp_min, "vp_max": vp_max, "vs_min": vs_min,
+                      "vs_max": vs_max, "rho_min": rho_min, "rho_max": rho_max,
                       }
-    return lines[:4], lines[4:], parsed_header
+
+            data = np.array([_.strip().split() for _ in lines[4:]], dtype=float)
+            if save:
+                np.save(xyz_fid, data)  # save data in .npy
+                np.savez(xyz_fid, **header)  # save header in .npz
+
+        return header, data
 
 
-def determine_checkers(data_list, bounds, spacing_m=50000, include_depth=False):
+def determine_checkers(data, header, spacing_m=50000):
     """
     read files in, define bounds, return
-    :type data_list: list
-    :param data_list: data read in using xyz_reader
-    :type bounds: dict
-    :param bounds: the bounds for each coordinate, e.g. {"x": [x_min, xmax]...}
-    :type data_out: list of str
-    :param data_out: if path should be writen
+    :type data: np.ndarray
+    :param data: data read in using xyz_reader
+    :type header: dict
+    :param header: the header from xyz_reader
     :type spacing_m: int
     :param spacing_m: spacing of checkers in meters
-    :type include_depth: bool
-    :param include_depth: checker in the z/depth coordinate
-    :rtype ischeckers: list
-    :return ischeckers: bool telling if a line is + or - checker, 0 or 1
+    :rtype checkerboard: np.array
+    :return checkerboard: the checkerboard the same size as the input data
     """
-    def define_bounds(value, var_bounds):
+    def define_checkers(point, min_bound, max_bound, checker_size, grid_size):
         """
-        x and y bounds are shared by each of the layers: shallow, crust, mantle
-        :type value: float
-        :param value: value to be evaluated
-        :type var_bounds: list of floats
-        :param var_bounds: [min_bound, max_bound]
-        :rtype: bool
-        :return: bool to say if the value falls within a checker or not
-        """
-        # defining z bounds requires extra information that x and y don't need
-        min_value = var_bounds[0]
-        max_value = var_bounds[1]
+        1-D operation
+        Given a point along an axis, first determine whether this point exists
+        in a positive or negative checker, and then determine where along a
+        tapering inside this checker the point exists.
 
-        checker_bool = True
+        :type point: float
+        :param point: coordinate point to be evaluated
+        :type min_bound: float
+        :param min_bound: minimum domain boundary
+        :type max_bound: float
+        :param max_bound: maximum domain boundary
+        :type checker_size: float
+        :param checker_size: the spacing of the overall domain
+        :type grid_size: float
+        :param grid_size: the grid spacing along the given axis
+        :rtype: int
+        :return: value of the checker for the given information
+        """
+        sign = 1
         # check if the value is between
-        for bound in np.arange(min_value, max_value, spacing_m):
-            if bound <= value < bound + spacing_m:
-                return checker_bool
-            checker_bool = not checker_bool
+        for inner_bound in np.arange(min_bound, max_bound, checker_size):
+            outer_bound = inner_bound + checker_size
+            if inner_bound <= point < outer_bound:
+                # Define the extent of the checker grid
+                checker = np.arange(inner_bound, outer_bound, grid_size)
 
-        return checker_bool
+                # Taper the checker (NOTE: Different signals can be used here)
+                taper_window = signal.tukey(len(checker))
+                taper_amount = taper_window[np.where(point == checker)[0][0]]
 
-    # go through each data file
-    ischecker = [[] for _ in range(len(data_list))]
-    for i, data in enumerate(data_list):
-        for line in data:
-            x_bool = define_bounds(float(line.strip().split()[0]), bounds["x"])
-            y_bool = define_bounds(float(line.strip().split()[1]), bounds["y"])
+                # Assign value based on the sign of the checker and tapering
+                checker_value = sign * taper_amount
 
-            # we don't always want to include checkering to depth because
-            # that affects turning of rays
-            if include_depth:
-                z_bool = define_bounds(
-                    float(line.strip().split()[2]), bounds["z"])
-                # checker only if all coordinates are true
-                # OR if all components are false
-                if x_bool == y_bool == z_bool:
-                    checker = 1
-                else:
-                    checker = -1
-            else:
-                if x_bool == y_bool:
-                    checker = 1
-                else:
-                    checker = -1
-            ischecker[i].append(checker)
+                return checker_value
+            # If the value is not within these bounds, switch sign and move on
+            sign *= -1
+        else:
+            # Takes the last value of the inner and outer bound
+            print("this shouldn't have happened")
+            return
 
-    return ischecker
+    # Go through each line in the file and determine the checker value
+    x_grid, y_grid, checkerboard_out = [], [], []
+    for gridpoint in data:
+        x_checker = define_checkers(point=gridpoint[0],  # this is the x value
+                                    min_bound=header["orig_x"],
+                                    max_bound=header["end_x"],
+                                    checker_size=spacing_m,
+                                    grid_size=header["spacing_x"]
+                                    )
+        y_checker = define_checkers(point=gridpoint[1],  # this is the y value
+                                    min_bound=header["orig_y"],
+                                    max_bound=header["end_y"],
+                                    checker_size=spacing_m,
+                                    grid_size=header["spacing_y"]
+                                    )
+        checkerboard_out.append(x_checker * y_checker)
+
+    return np.array(checkerboard_out)
 
 
-def checkerboardiphy(data, ischeckers, perturbation=0.1):
+def convert_to_checkers(data, checkerboard, perturbation=0.1):
     """
     take checker boolean data and perturb base model with given perturbation
 
@@ -115,73 +141,120 @@ def checkerboardiphy(data, ischeckers, perturbation=0.1):
 
     :return:
     """
-    for i, line in enumerate(data):
-        (x, y, z, vp, vs, rho, qp, qs) = line.split()
-        perturb = ischeckers[i] * perturbation
-        vp = float(vp) + (float(vp) * perturb)
-        vs = float(vs) + (float(vs) * perturb)
-        rho = float(rho) + (float(rho) * perturb)
-        qp = float(qp) + (float(qp) * perturb)
-        qs = float(qs) + (float(qs) * perturb)
-        data[i] = (
-            "{} {} {} {:.1f} {:.1f} {:.1f} {:.1f} {:.1f}\n".format(
-                x, y, z, vp, vs, rho, qp, qs)
-        )
+    data_out = np.copy(data)
+    for i, gridpoint in enumerate(data):
+        # Set the value of the perturbation to add to the gridpoint
+        perturb = checkerboard[i] * perturbation
+        for j, value in gridpoint:
+            # Make sure we skip over the coordinates, which occupy first 3
+            if j in np.arange(0, 3):
+                continue
+            # New data is old data plus some perturbation of old data
+            data_out[i] = data_in[i] + data_in[i] * perturb
 
-    return data
+    return data_out
 
 
-def write_out(header, data, file_id):
+def checkerboardiphy(xyz_fid, spacing_m, perturbation=0.02):
     """
-    simple writer to create new tomography files
-    :param header:
-    :param data:
-    :param file_id:
+    Amalgamation of the above functions
+
+    This is too slow, doesn't work
+
+    :type xyz_fid: str
+    :param xyz_fid: path to file to read
+    :type spacing_m: float
+    :param spacing_m: spacing in meters
+    :type perturbation: float
+    :param perturbation: perturbation to give to checkers
     :return:
     """
-    with open(file_id, "w") as f:
-        for h in header:
-            f.write(h)
-        for d in data:
-            f.write(d)
+    # Read in the data
+    header, data = xyz_reader(xyz_fid=xyz_fid, save=True)
+
+    # Create the checkerboard based on the given data
+    checkerboard = determine_checkers(data, header, spacing_m)
+
+    # Convert the data into checkerboard
+    data_out = convert_to_checkers(data, checkerboard, perturbation)
+
+    return data_out
+
+
+# def checkerboardiphy(xyz_fid, spacing_m, perturbation=0.02):
+#     """
+#     Amalgamation of the above functions
+#
+#     :type xyz_fid: str
+#     :param xyz_fid: path to file to read
+#     :type spacing_m: float
+#     :param spacing_m: spacing in meters
+#     :type perturbation: float
+#     :param perturbation: perturbation to give to checkers
+#     :return:
+#     """
+#     # Read in the data
+#     header, data = xyz_reader(xyz_fid=xyz_fid, save=True)
+#
+#     data_out = np.copy(data)
+#     x, y = 1, 1
+#     # Loop through the x-axis
+#     for x_left in np.arange(header["orig_x"], header["end_x"], spacing_m):
+#         x_right = x_left + spacing_m
+#         # Create the tapered checker for the x-axis
+#         x_checker = np.arange(x_left, x_right, header["spacing_x"])
+#         x_window = signal.tukey(len(x_checker))
+#
+#         # Loop through the y-axis
+#         for y_bot in np.arange(header["orig_y"], header["end_y"], spacing_m):
+#             y_top = y_bot + spacing_m
+#             # Create the tapered checker for the x-axis
+#             y_checker = np.arange(y_bot, y_top, header["spacing_y"])
+#             y_window = signal.tukey(len(y_checker))
+#
+#             # The sign of the overall checker
+#             checker = x * y
+#
+#             # Determine where the data falls within this checker
+#             import ipdb;ipdb.set_trace()
+#
+#             data_out[:, 0] = np.where(
+#                 (data[:, 0] > x_right) & (data[:, 0] <= x_left),
+#                 data[:, 0],
+#             )
+#
+#             checker_indices = np.where((x_right < data[:, 0]) &
+#                                        (data[:, 0] <= x_left) &
+#                                        (y_bot < data[:, 1]) &
+#                                        (data[:, 1] <= y_top)
+#                                        )
+#
+#
+#             data[checker_indices]
+#             taper_amount = taper_window[np.where(point == checker)[0][0]]
+#
+#             y *= -1
+#         x *= -1
+#
+#
+#     return data_out
 
 
 if __name__ == "__main__":
-    # read in data
-    # path = "/Users/chowbr/Documents/subduction/data/KUPEDATA/tomo_files"
-    path = "/seis/prj/fwi/bchow/data/KUPEDATA/tomo_files" 
-    name_template = "nz_x1200_y600_eberhart2015_{}.xyz"
-    fullpath = os.path.join(path, name_template)
+    path = "./"
+    fid_template = "nz_north_eberhart2015_{}.xyz"
+    for section in ["shallow", "crust", "header"]:
+        fid = fid_template.format(section)
+        checkerboard_data = checkerboardiphy(xyz_fid=os.path.join(path, fid),
+                                             spacing_m=50000.,
+                                             perturbation=0.02
+                                             )
+        # save the data with a new tag
+        fid_out = fid.split('.')[0] + "_checker." + fid.split('.')[1]
+        np.save(checkerboard_data, fid_out)
 
-    shallow_header, shallow_data, shallow_parsed = xyz_reader(
-        fullpath.format("shallow"))
-    crust_header, crust_data, crust_parsed = xyz_reader(
-        fullpath.format("crust"))
-    mantle_header, mantle_data, mantle_parsed = xyz_reader(
-        fullpath.format("mantle"))
-    data_list = [shallow_data, crust_data, mantle_data]
 
-    # user defined paramters
-    spacing_m = 50000.
-    perturbation = 0.1
 
-    # workflow
-    bounds_dict = {"x": [shallow_parsed["orig_x"], shallow_parsed["end_x"]],
-                   "y": [shallow_parsed["orig_y"], shallow_parsed["end_y"]],
-                   "z": [mantle_parsed["orig_z"], shallow_parsed["end_z"]]
-                   }
-    ischeckers = determine_checkers(
-        data_list, bounds_dict, spacing_m=spacing_m, include_depth=False
-    )
-
-    headers = [shallow_header, crust_header, mantle_header]
-    data_out = [fullpath.format("shallow_checker"),
-                fullpath.format("crust_checker"),
-                fullpath.format("mantle_checker")
-                ]
-    for i, data in enumerate(data_list):
-        new_data = checkerboardiphy(data, ischeckers[i], perturbation)
-        write_out(headers[i], new_data, data_out[i])
 
 
 
--- simulations/meshing/trim_xyz_tomo_file.py
+++ simulations/meshing/trim_xyz_tomo_file.py
@@ -1,50 +1,7 @@
 import os
 import numpy as np
 
-
-def xyz_reader(filepath):
-    """
-    read .xyz file used as input for specfem and return information
-    header parsing specifically related to specfem
-    data is delineated as :
-    x, y, z, vp[m/s], vs[m/s], rho[kg/m**3], Qp, Qs
-    -origin and endpoints are the coordinates in meters
-    -spacing given in units of meters for UTM projections
-    -nx, ny, nz = [(end_x - orig_x)/spacing_x] + 1
-    :param filepath:
-    :return:
-    """
-    
-    if os.path.exists(filepath + ".npy") and os.path.exists(filepath + ".npz"):
-        header = np.load(filepath + ".npz")
-        data = np.load(filepath + ".npy")
-        return header, data
-    else:
-        with open(filepath) as f:
-            lines = f.readlines()
-
-            # parse the header and make sure the values are floats
-            orig_x, orig_y, orig_z, end_x, end_y, end_z = \
-                [float(_) for _ in lines[0].strip().split()]
-            spacing_x, spacing_y, spacing_z = \
-                [float(_) for _ in lines[1].strip().split()]
-            nx, ny, nz = [float(_) for _ in lines[2].strip().split()]
-            vp_min, vp_max, vs_min, vs_max, rho_min, rho_max = \
-                [float(_) for _ in lines[3].strip().split()]
-
-            header = {"orig_x": orig_x, "orig_y": orig_y, "orig_z": orig_z,
-                      "end_x": end_x, "end_y": end_y, "end_z": end_z,
-                      "spacing_x": spacing_x, "spacing_y": spacing_y,
-                      "spacing_z": spacing_z, "nx": nx, "ny": ny, "nz": nz,
-                      "vp_min": vp_min, "vp_max": vp_max, "vs_min": vs_min,
-                      "vs_max": vs_max, "rho_min": rho_min, "rho_max": rho_max,
-                     }
-            
-            data = np.array([_.strip().split() for _ in lines[4:]], dtype=float)
-            np.save(filepath, data)  # save data in .npy
-            np.savez(filepath, **header)  # save header in .npz
-
-        return header, data
+from xyz_reader import xyz_reader
 
 
 def trim_xyz_file(data, bounds):
@@ -79,7 +36,6 @@ def trim_xyz_file(data, bounds):
                          "rho_max": data[:, 5].max(),
                       }
 
-  
     head_len = parsed_header["nx"] * parsed_header["ny"] * parsed_header["nz"] 
     assert head_len == len(data)
 
@@ -122,8 +78,9 @@ def write_new_xyz(header, data, fidout_template):
 
 if __name__ == "__main__":
     # path = "/Users/chowbr/Documents/subduction/data/KUPEDATA/tomo_files"
-    path = "/seis/prj/fwi/bchow/data/KUPEDATA/tomo_files"
-    name_template = "nz_full_eberhart2015_{}.xyz"
+    # path = "/seis/prj/fwi/bchow/data/KUPEDATA/tomo_files"
+    path = "./"
+    name_template = "nz_north_eberhart2015_{}.xyz"
     fullpath = os.path.join(path, name_template)
    
     # output file names
@@ -140,12 +97,12 @@ if __name__ == "__main__":
     bounds = [x_bounds, y_bounds, z_bounds]
 
     # trim xyz files based on given bounds
-    for i, section in enumerate(sections):  #, crust_data, mantle_data]:
+    for i, section in enumerate(sections):
         print("trimming section {}".format(section))
         header, data = xyz_reader(fullpath.format(section))
         new_header, new_data = trim_xyz_file(data, bounds)
         outpath_temp = outpath.format(x="{x:.0f}", y="{y:.0f}", section=section)
-        write_new_xyz(new_header, new_data, outpath_temp) 
+        write_new_xyz(new_header, new_data, outpath_temp)
         
 
 
