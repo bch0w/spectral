20e501cd2d78b70f501605d14883f0aad3888d34
Bryant Chow <bryant.h.c@gmail.com>
2019-11-04 11:10:15 +1300

    data availability plot works for individual years

--- observations/data_availability.py
+++ observations/data_availability.py
@@ -6,12 +6,40 @@ import os
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def myround(x, base=5, choice='near'):
+    """
+    Round value x to nearest base, round 'up','down' or to 'near'est base
+
+    :type x: float
+    :param x: value to be rounded
+    :type base: int
+    :param base: nearest integer to be rounded to
+    :type choice: str
+    :param choice: method of rounding, 'up', 'down' or 'near'
+    :rtype roundout: int
+    :return: rounded value
+    """
+    if choice == 'near':
+        roundout = int(base * round(float(x)/base))
+    elif choice == 'down':
+        roundout = int(base * np.floor(float(x)/base))
+    elif choice == 'up':
+        roundout = int(base * np.ceil(float(x)/base))
+
+    return roundout
+
+
+# define service dates
 
 
 path = "./"
 years = glob.glob(os.path.join(path, "????"))
 years.sort()
 for year in years:
+    earliest, latest = 1E3, 0
     netstas = []
     filled_years = None
     networks = glob.glob(os.path.join(year, "*"))
@@ -29,6 +57,11 @@ for year in years:
                 # File ids will add to the bit in the full year
                 for fileid in fileids:
                     jday = int(fileid.split('.')[-1])
+                    # determine what the start and end dates are
+                    if jday < earliest:
+                        earliest = jday
+                    elif jday > latest:
+                        latest = jday
                     station_full_year[jday] += 1
 
             netstaname = "{}.{}".format(os.path.basename(network),
@@ -39,15 +72,35 @@ for year in years:
             else:
                 filled_years = np.vstack([filled_years, station_full_year])
 
-        fig, ax = plt.subplots(figsize=(10,10)) 
-        im = ax.imshow(filled_years, aspect="auto")
-        ax.set_xticks(np.arange(len(station_full_year)))
+        # reduce data to the available bound
+        filled_years = filled_years[:, earliest:latest]
+
+        fig, ax = plt.subplots(figsize=(20,10)) 
+        im = ax.imshow(filled_years, extent=(earliest, latest, len(netstas), 0),
+                       aspect="auto", cmap="Oranges")
+        ax.set_title("{} BEACON DATA AVAILABILITY".format(
+                                                        os.path.basename(year)))
+
+        # colorbar
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes('right', size='5%', pad=0.05)
+        fig.colorbar(im, cax=cax, orientation='vertical', values=[0,1,2,3],
+                     ticks=[0,1,2,3])
+        
+        # grid
+        ax.grid(True, which='major', linestyle='-', c="k", linewidth=1.25)
+        ax.grid(True, which='minor', linestyle='-', c="k", linewidth=0.5, 
+                alpha=0.5)
+        ax.set_xticks(np.arange(myround(earliest, 10, 'near'), latest, 10))
+        ax.set_xticks(np.arange(myround(earliest, 10, 'near'), latest, 1), 
+                      minor=True)
         ax.set_yticks(np.arange(len(netstas)))
         ax.set_yticklabels(netstas)
-        plt.setp(ax.get_xticklabels(), rotation=90)
-        plt.grid()
-        ax.set_title("{} DATA".format(os.path.basename(year)))
-        plt.savefig("data_{}.png".format(os.path.basename(year)))
+        plt.setp(ax.get_xticklabels(), rotation=45)
+        plt.setp(ax.get_yticklabels(), rotation=45)
+
+        plt.show()
+        # plt.savefig("data_{}.png".format(os.path.basename(year)))
 
 
 
