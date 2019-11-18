abdc72b0fc844a60aa6130855e2ba40be5c10e0b
Bryant Chow <bryant.h.c@gmail.com>
2019-11-19 09:58:14 +1300

    waveform breakdown figure working well

--- publications/waveform_breakdown.py
+++ publications/waveform_breakdown.py
@@ -1,14 +1,45 @@
+"""
+Creating waveform plots for illustrative purposes, so less labels,
+bigger linesizes etc
+"""
+import os
 import pyatoa
 import pyasdf
 
-station = "NZ.MRZ.??.HH?"
+choice = "southern"
+
+if choice == "inland":
+    event_id = "2016p275188"
+    sta_list = ["BKZ", "RTZ", "KHEZ", "KHZ"]
+elif choice == "inland_2":
+    event_id = "2019p754447"
+    sta_list = ["HIZ"] # "TLZ", "TOZ"]
+elif choice == "forearc_1":
+    event_id = "2016p881118"
+    sta_list = ["KNZ", "MXZ"]
+elif choice == "forearc_2":
+    event_id = "2015p768477"
+    sta_list = ["KNZ", "PUZ", "MXZ"]
+elif choice == "forearc_3":
+    event_id = "3367989"
+    sta_list = ["PUZ"] #"KNZ", "MXZ"]
+elif choice == "forearc_back":
+    event_id = "2019p738432"
+    sta_list = ["PXZ"] # , "BFZ", "KHZ"]
+elif choice == "volcano":
+    event_id = "2014p240655"
+    sta_list = ["TLZ"] # , "TOZ", "MKAZ"]
+elif choice == "southern":
+    event_id = "3620927"
+    sta_list = ["NNZ", "MRZ", "KHZ"]
 
 config = pyatoa.Config(
-    event_id="2019p754447",
+    event_id=event_id,
     model_number="m00",
     min_period=10,
     max_period=30,
     filter_corners=4,
+    component_list=["Z"],
     rotate_to_rtz=False,
     unit_output="DISP",
     pyflex_map="hikurangi",
@@ -16,17 +47,23 @@ config = pyatoa.Config(
     cfgpaths={"waveforms": [], "synthetics": './', "responses": []}
 )
 
-ds = pyasdf.ASDFDataSet(f"{config.event_id}.h5")
+assert(os.path.exists(f"./hdf5/{config.event_id}.h5")), "event id doesn't exist"
+ds = pyasdf.ASDFDataSet(f"./hdf5/{config.event_id}.h5")
 
-mgmt = pyatoa.Manager(config=config, ds=ds)
-mgmt.populate(station_code=station, model=config.model_number)
-mgmt.preprocess()
-mgmt.run_pyflex()
-mgmt.run_pyadjoint()
+for sta in sta_list:
+    station = f"NZ.{sta}.??.HH?"
+    mgmt = pyatoa.Manager(config=config, ds=ds)
+    mgmt.populate(station_code=station, model=config.model_number)
+    mgmt.st_obs = mgmt.st_obs.select(component="Z")
+    mgmt.preprocess()
+    mgmt.run_pyflex()
+    mgmt.run_pyadjoint()
 
-
-mgmt.plot_wav(show=True, length_sec=200, axes_linewidth=4, linewidth=2.5, 
-              fontsize=0, window_anno_fontsize=0, window_anno_height=0.4, 
-              window_anno_rotation=0, window_anno_fontcolor="k",
-              window_anno_fontweight="roman", legend=False)
+    mgmt.plot_wav(show=True, figsize=(10,4), length_sec=180, fontsize=14,
+                  axes_linewidth=4, linewidth=2.5, window_anno_fontsize=0, 
+                  window_anno_height=0.4, window_anno_rotation=0, 
+                  window_anno_fontcolor="k", window_anno_fontweight="roman", 
+                  plot_waterlevel=False, plot_window_anno=False, 
+                  plot_windows=False, plot_adjsrc=False, plot_stalta=False,
+                  legend=False)
 
