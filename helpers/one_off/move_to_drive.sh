f2803cee794d4d79de64c3fb1fde24c31cb155d5
Bryant Chow <bryant.h.c@gmail.com>
2020-07-27 10:16:44 +1200

    MAJOR: removed a bunch of random fluff/one-off scripts, moved some directories around, hopefully will start using this repo a bit more now that its cleaner

--- helpers/one_off/move_to_drive.sh
+++
@@ -1,4 +0,0 @@
-cp -rn /seis/prj/fwi/bchow/spectral/common/DATA/QUAKEML/ /run/media/bchow/convolution/common/DATA
-cp -rn /seis/prj/fwi/bchow/spectral/common/DATA/STATIONXML/ /run/media/bchow/convolution/common/DATA
-
-cp -rn ~/Documents/papers/ /run/media/bchow/convolution/
