6e64782460e722e0bae549aa9daaef407b36bb9b
bch0w <bryant.h.c@gmail.com>
2023-01-24 19:14:56 -0900

    added polar plot to declustering run script

--- alaska/akatom/decluster/run_decluster_events.py
+++ alaska/akatom/decluster/run_decluster_events.py
@@ -13,12 +13,24 @@ inv = read_inventory("../metadata/nalaska_stations.xml")
 zedges = [0, 10, 35, 100]
 
 declust = Declust(cat=cat, inv=inv)
+
+# Plot the original event catalog
+declust.plot(cat=cat, inv=inv, color_by="depth", vmin=0, vmax=30, 
+             cmap="inferno_r",
+             show=False, save="og_event_catalog.png", 
+             title=f"Original Event Catalog (N={len(cat)})")
+
 declust.threshold_events(zedges=zedges, min_mags=[3., 4., 5.], min_data=10)
 
 # Run through once with data availability
 newcat = declust.decluster_events(choice="cartesian", zedges=zedges, 
                                   nkeep=[3, 3, 3], select_by="data_r", 
                                   nx=30, ny=30, plot=True)
+
+
+polcat = declust.decluster_events(choice="polar", zedges=zedges, 
+                                  nkeep=[3, 3, 3], select_by="data_r", 
+                                  ntheta=32, plot=True)
 
 
 declust.plot(cat=newcat, inv=inv, color_by="data", vmin=1, cmap="viridis",
