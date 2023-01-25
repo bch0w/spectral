9780d88a1d6823e7ae6bf47669ea027f00f430f0
bch0w <bryant.h.c@gmail.com>
2023-01-24 18:25:47 -0900

    pysep declustering script for event catalog

---
+++ alaska/akatom/decluster/run_decluster_events.py
@@ -0,0 +1,29 @@
+"""
+Using PySEP to curate the event catalog
+"""
+from obspy import read_events, read_inventory
+from pysep import Declust, logger
+
+logger.setLevel("DEBUG")
+
+
+cat = read_events("../metadata/nealaska_events.xml")
+inv = read_inventory("../metadata/nalaska_stations.xml")
+
+zedges = [0, 10, 35, 100]
+
+declust = Declust(cat=cat, inv=inv)
+declust.threshold_events(zedges=zedges, min_mags=[3., 4., 5.], min_data=10)
+
+# Run through once with data availability
+newcat = declust.decluster_events(choice="cartesian", zedges=zedges, 
+                                  nkeep=[3, 3, 3], select_by="data_r", 
+                                  nx=30, ny=30, plot=True)
+
+
+declust.plot(cat=newcat, inv=inv, color_by="data", vmin=1, cmap="viridis",
+             show=False, save="decluster_data_avail.png", 
+             connect_data_avail=True,
+             title="Declustered Event Catalog Data Availability")
+
+
