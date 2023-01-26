32a2acd0a6fb0c2bb9d8fd1bcdc98372cd47250f
Bryant Chow <bryant.h.c@gmail.com>
2023-01-26 14:06:24 -0900

    small update run decluster

--- alaska/akatom/decluster/run_decluster_events.py
+++ alaska/akatom/decluster/run_decluster_events.py
@@ -7,7 +7,7 @@ from pysep import Declust, logger
 logger.setLevel("DEBUG")
 
 
-cat = read_events("../metadata/nealaska_events.xml")
+cat = read_events("../metadata/nalaska_events.xml")
 inv = read_inventory("../metadata/nalaska_stations.xml")
 
 zedges = [0, 10, 35, 100]
@@ -20,17 +20,22 @@ declust.plot(cat=cat, inv=inv, color_by="depth", vmin=0, vmax=30,
              show=False, save="og_event_catalog.png", 
              title=f"Original Event Catalog (N={len(cat)})")
 
+declust.plot(cat=cat, inv=inv, color_by="data", vmin=0, vmax=30, 
+             cmap="viridis",
+             show=False, save="og_data_avail.png", 
+             title=f"OG CAT DATA AVAIALABILITY (N={len(cat)})")
+
 declust.threshold_events(zedges=zedges, min_mags=[3., 4., 5.], min_data=10)
 
-# Run through once with data availability
+# Select by data availability
 newcat = declust.decluster_events(choice="cartesian", zedges=zedges, 
                                   nkeep=[3, 3, 3], select_by="data_r", 
                                   nx=30, ny=30, plot=True)
 
-
-polcat = declust.decluster_events(choice="polar", zedges=zedges, 
-                                  nkeep=[3, 3, 3], select_by="data_r", 
-                                  ntheta=32, plot=True)
+if False:
+    polcat = declust.decluster_events(choice="polar", zedges=zedges, 
+                                      nkeep=[3, 3, 3], select_by="data_r", 
+                                      ntheta=32, plot=True)
 
 
 declust.plot(cat=newcat, inv=inv, color_by="data", vmin=1, cmap="viridis",
