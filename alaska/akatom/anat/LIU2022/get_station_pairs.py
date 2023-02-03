2dbae4aec2731138437aac894c270fbcae297a51
Bryant Chow <bryant.h.c@gmail.com>
2023-02-03 12:12:51 -0900

    adding anat prelim work

---
+++ alaska/akatom/anat/LIU2022/get_station_pairs.py
@@ -0,0 +1,49 @@
+"""
+Get station pair information from the CC stacks based on file names from the 
+3-station EGF from
+
+Liu et al. (2022)
+    https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2022JB024885
+
+Files:
+    https://doi.org/10.5281/zenodo.7080282
+"""
+import os 
+from glob import glob
+from obspy import read
+
+
+netstas_a = []
+netstas_b = []
+lats = dict()
+lons = dict()
+with open("station_pairs.txt", "w") as f:
+    for dir_ in glob("*_stack"):
+        phase, _, typ, _ = dir_.split("_")
+        for dir_ in sorted(glob(os.path.join(dir_, "*_*"))):
+            for i, fullpath in enumerate(sorted(glob(os.path.join(dir_, "*")))):
+                fid = os.path.basename(fullpath)
+                fid = os.path.splitext(fid)[0]
+                _, net_a, sta_a, net_b, sta_b = fid.strip().split("_")
+                if i == 0:
+                    f.write(f"{phase.upper()}  {typ.upper()}  {net_a}.{sta_a}\n")
+                f.write(f"\t{net_b}.{sta_b}\n")
+
+                netsta_a = f"{net_a} {sta_a}"
+                netsta_b = f"{net_b} {sta_b}"
+                if netsta_a not in netstas_a:
+                    netstas_a.append(netsta_a)
+                if netsta_b not in netstas_b:
+                    netstas_b.append(netsta_b)
+                    st = read(fullpath)
+                    lats[netsta_b] = st[0].stats.sac.stla
+                    lons[netsta_b] = st[0].stats.sac.stlo
+
+
+assert(len(netstas_a) == len(netstas_b))
+
+with open("STATIONS_LIU2022", "w") as f:
+    for netsta in sorted(netstas_b):
+        net, sta = netsta.split(" ")
+        f.write(f"{net:>6}{sta:>6}{lats[netsta]:11.4f}{lons[netsta]:11.4f}{0:7.1f}{0:7.1f}\n")
+
